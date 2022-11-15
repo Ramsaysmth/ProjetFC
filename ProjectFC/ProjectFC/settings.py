7bc3655727d6d07d178ac4de413699f945cf3dd8
Guillaume <olivieri.guillaume@yahoo.fr>
2022-11-15 22:56:05 +0100

    Patch secret key et debug mode

--- ProjectFC/ProjectFC/settings.py
+++ ProjectFC/ProjectFC/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4qus0t%mlzbx-jbrks$y-19wpz@a_xpag7_9_kzw#ff07ph1^z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
